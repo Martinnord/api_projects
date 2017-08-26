9acbf4df57b623ba2f2a646c72d2cc8608dcaebb
martinnord <martin.nordstrom99@gmail.com>
2017-08-26 03:29:53 +0200

    added comments

--- timeStamp/app.js
+++ timeStamp/app.js
@@ -1,40 +1,45 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const port = 3000
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/dates/:dateValue', (req, res, next) => {
+app.get('/dates/:dateValue', (req, res) => {
   const dateValue = req.params.dateValue
+  console.log(req.params)
   
   const formatDate = {
     year: 'numeric',
     month: 'long',
     day: 'numeric'
   }
-  
+
+  // Checks if the datevalue is a valid number
   if(isNaN(dateValue)) {
+    // For the natural date
     var naturalDate = new Date(dateValue)
     naturalDate = naturalDate.toLocaleDateString('en-us', formatDate)
-    
     var unixDate = new Date(dateValue).getTime()/1000
   } else {
-    var unixDate = dateValue
-    var naturalDate = new Date(dateValue * 1000)
-    naturalDate = naturalDate.toLocaleDateString('en-us', formatDate)
+      // For the unix date
+      var unixDate = dateValue
+      var naturalDate = new Date(dateValue * 1000)
+      naturalDate = naturalDate.toLocaleDateString('en-us', formatDate)
   }
   
+  // Creates a JSON object
   res.json({
     unix: unixDate,
     natural: naturalDate
   })
 })
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000!")
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
 })
 
 module.exports = app
